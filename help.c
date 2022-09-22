94878214eeb671646ab196b078d619786aa1909f
Rajat Varma <rajatvarma08@gmail.com>
2022-09-22 13:41:40 +0530

    some c code - uploaded due to debayan threat

---
+++ help.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int i;
+    puts("Printing integers from 1 to 5");
+    for (i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
